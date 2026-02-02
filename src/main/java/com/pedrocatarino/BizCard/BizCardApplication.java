8931dbf8bcfd8b4fee016bd933d4c3d73876b80b
pedro <pedrocatarino246@gmail.com>
2026-02-02 16:16:38 -0300

    Create project

---
+++ src/main/java/com/pedrocatarino/BizCard/BizCardApplication.java
@@ -0,0 +1,13 @@
+package com.pedrocatarino.BizCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizCardApplication.class, args);
+	}
+
+}
